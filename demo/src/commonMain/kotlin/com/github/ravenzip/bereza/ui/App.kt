280b67d1ee0a9b01b28755c5a5925a0c490e541e
RavenZIP <razor01478920@mail.ru>
2026-02-02 21:10:58 +0300

    Сырая демка, чтобы пощупать проект

--- demo/src/commonMain/kotlin/com/github/ravenzip/bereza/ui/App.kt
+++ demo/src/commonMain/kotlin/com/github/ravenzip/bereza/ui/App.kt
@@ -1,19 +1,50 @@
+package com.github.ravenzip.bereza.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import components.textfield.singleLine.SingleLineTextField
-import form.mutableFormControl
+import androidx.compose.ui.unit.dp
+import com.github.ravenzip.kotlinreactiveforms.form.mutableFormControl
+import com.github.ravenzip.kotlinreactiveforms.validation.Validator
+import components.button.SimpleButton
+import components.checkbox.Checkbox
+import components.textfield.singleLine.OutlinedSingleLineTextField
 
 @Composable
 fun App() {
     MaterialTheme {
         val scope = rememberCoroutineScope()
-        val control = remember { mutableFormControl("", coroutineScope = scope) }
+        val control1 = remember {
+            mutableFormControl("", validators = listOf(Validator.required), coroutineScope = scope)
+        }
 
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> SingleLineTextField(control) }
+        val control2 = remember {
+            mutableFormControl("", validators = listOf(Validator.required), coroutineScope = scope)
+        }
+
+        val control3 = remember { mutableFormControl(false, coroutineScope = scope) }
+
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding).padding(start = 10.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+            ) {
+                OutlinedSingleLineTextField(control1, label = { Text("Текстовое поле 1") })
+
+                OutlinedSingleLineTextField(control2, label = { Text("Текстовое поле 2") })
+
+                Checkbox(control = control3, text = "Я чекбокс")
+
+                SimpleButton(onClick = { control1.setValue("Значение") }, text = "Кнопка")
+            }
+        }
     }
 }
