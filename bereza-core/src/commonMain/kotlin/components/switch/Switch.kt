5ad525670607306e53bdb1890c462bd524541332
RavenZIP <razor01478920@mail.ru>
2026-02-21 10:56:06 +0300

    Свич с текстом переименовал в SwitchWithText, сделал отдельный компонент свича без текста (на основе FormControl)

--- bereza-core/src/commonMain/kotlin/components/switch/Switch.kt
+++ bereza-core/src/commonMain/kotlin/components/switch/Switch.kt
@@ -1,147 +1,31 @@
 package components.switch
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.SwitchDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.github.ravenzip.kotlinreactiveforms.data.isEnabled
 import com.github.ravenzip.kotlinreactiveforms.form.MutableFormControl
 
 @Composable
 fun Switch(
-    isSelected: Boolean,
-    isEnabled: Boolean = true,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    text: @Composable () -> Unit,
-    padding: PaddingValues = PaddingValues(15.dp),
-    colors: SwitchColors = SwitchDefaults.colors(),
-    shape: Shape = RoundedCornerShape(14.dp),
-) {
-    Row(
-        modifier = modifier.clip(shape).clickable { onClick() }.padding(padding),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        text()
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Switch(
-            checked = isSelected,
-            onCheckedChange = { onClick() },
-            enabled = isEnabled,
-            colors = colors,
-        )
-    }
-}
-
-@Composable
-fun Switch(
-    isSelected: Boolean,
-    isEnabled: Boolean = true,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    label: String,
-    labelStyle: TextStyle =
-        TextStyle.Default.merge(fontSize = 16.sp, fontWeight = FontWeight.Medium),
-    description: String? = null,
-    descriptionStyle: TextStyle? = null,
-    padding: PaddingValues = PaddingValues(15.dp),
-    colors: SwitchColors = SwitchDefaults.colors(),
-    shape: Shape = RoundedCornerShape(14.dp),
-) {
-    Switch(
-        isSelected = isSelected,
-        isEnabled = isEnabled,
-        onClick = { onClick() },
-        modifier = modifier,
-        text = {
-            if (description != null && descriptionStyle != null) {
-                Column {
-                    Text(text = label, style = labelStyle)
-
-                    Text(text = description, style = descriptionStyle)
-                }
-            } else {
-                Text(text = label, style = labelStyle)
-            }
-        },
-        padding = padding,
-        colors = colors,
-        shape = shape,
-    )
-}
-
-@Composable
-fun Switch(
     control: MutableFormControl<Boolean>,
     modifier: Modifier = Modifier,
-    text: @Composable () -> Unit,
-    padding: PaddingValues = PaddingValues(15.dp),
     colors: SwitchColors = SwitchDefaults.colors(),
-    shape: Shape = RoundedCornerShape(14.dp),
 ) {
     val isSelected = control.valueChanges.collectAsState().value
     val status = control.statusChanges.collectAsState().value
 
     Switch(
-        isSelected = isSelected,
-        isEnabled = status.isEnabled(),
-        onClick = {
+        checked = isSelected,
+        onCheckedChange = {
             control.setValue(!control.value)
             control.markAsDirty()
         },
         modifier = modifier,
-        text = text,
-        padding = padding,
+        enabled = status.isEnabled(),
         colors = colors,
-        shape = shape,
-    )
-}
-
-@Composable
-fun Switch(
-    control: MutableFormControl<Boolean>,
-    modifier: Modifier = Modifier,
-    label: String,
-    labelStyle: TextStyle =
-        TextStyle.Default.merge(fontSize = 16.sp, fontWeight = FontWeight.Medium),
-    description: String,
-    descriptionStyle: TextStyle = TextStyle.Default.merge(fontSize = 16.sp),
-    padding: PaddingValues = PaddingValues(15.dp),
-    colors: SwitchColors = SwitchDefaults.colors(),
-    shape: Shape = RoundedCornerShape(14.dp),
-) {
-    val isSelected = control.valueChanges.collectAsState().value
-    val status = control.statusChanges.collectAsState().value
-
-    Switch(
-        isSelected = isSelected,
-        isEnabled = status.isEnabled(),
-        onClick = {
-            control.setValue(!control.value)
-            control.markAsDirty()
-        },
-        modifier = modifier,
-        label = label,
-        labelStyle = labelStyle,
-        description = description,
-        descriptionStyle = descriptionStyle,
-        padding = padding,
-        colors = colors,
-        shape = shape,
     )
 }
