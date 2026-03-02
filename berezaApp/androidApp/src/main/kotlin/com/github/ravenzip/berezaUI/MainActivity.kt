9bef23299bbe785ec3726c5950451e6b7a1dfa70
Черных Александр <razor01478920@mail.ru>
2026-03-02 12:00:16 +0300

    Неиспользуемые импорты

--- berezaApp/androidApp/src/main/kotlin/com/github/ravenzip/berezaUI/MainActivity.kt
+++ berezaApp/androidApp/src/main/kotlin/com/github/ravenzip/berezaUI/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.github.ravenzip.bereza.ui.com.github.ravenzip.berezaUI.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
