c72d2bd53d11eeea8f4ecdea8ca2e4c0823affbd
Черных Александр <razor01478920@mail.ru>
2026-04-10 12:28:20 +0300

    Использование флага ignoreCase вместо ручного приведения к нижнему регистру

--- bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/data/autocomplete/AutocompleteSearch.kt
+++ bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/data/autocomplete/AutocompleteSearch.kt
@@ -1,13 +1,13 @@
 package com.github.ravenzip.berezaUI.core.data.autocomplete
 
 object AutocompleteSearch {
-    fun <T> startWith(source: List<T>, value: String, key: (T) -> String): List<T> {
-        val lowerCaseValue = value.lowercase()
-        return source.filter { item -> key(item).lowercase().startsWith(lowerCaseValue) }
-    }
+    fun <T> startWith(source: List<T>, value: String, key: (T) -> String): List<T> =
+        source.filter { item ->
+            key(item).startsWith(prefix = value, ignoreCase = true)
+        }
 
-    fun <T> contains(source: List<T>, value: String, key: (T) -> String): List<T> {
-        val lowerCaseValue = value.lowercase()
-        return source.filter { item -> key(item).lowercase().contains(lowerCaseValue) }
-    }
+    fun <T> contains(source: List<T>, value: String, key: (T) -> String): List<T> =
+        source.filter { item ->
+            key(item).contains(other = value, ignoreCase = true)
+        }
 }
