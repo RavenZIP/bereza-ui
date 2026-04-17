7c3ecdc2e9d14970342526a355bebbb9db0bdf38
RavenZIP <razor01478920@mail.ru>
2026-04-17 07:02:12 +0300

    Вынес RadioGroup с FormControl из extensions модуля в reactive модуль
    Добавил в core модуль радиогруппу

--- bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/components/radio/RadioGroup.kt
+++ bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/components/radio/RadioGroup.kt
@@ -1,19 +1,17 @@
-package com.github.ravenzip.berezaUI.extensions.components.radio
+package com.github.ravenzip.berezaUI.core.components.radio
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.RadioButtonColors
 import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.github.ravenzip.berezaUI.core.components.radio.RadioGroup
 
 @Composable
 fun <T, K : Any> RadioGroup(
@@ -22,26 +20,30 @@ fun <T, K : Any> RadioGroup(
     onSelectedItemChange: (T) -> Unit,
     keySelector: (T) -> K,
     modifier: Modifier = Modifier,
-    displayedText: (T) -> String,
-    displayedTextStyle: TextStyle =
-        TextStyle.Default.merge(fontSize = 16.sp, fontWeight = FontWeight.Medium),
+    text: @Composable (T) -> Unit,
     enabled: Boolean = true,
     contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
     padding: PaddingValues = PaddingValues(15.dp),
     shape: Shape = RoundedCornerShape(14.dp),
     colors: RadioButtonColors = RadioButtonDefaults.colors(),
 ) {
-    RadioGroup(
-        source = source,
-        selectedItem = selectedItem,
-        onSelectedItemChange = onSelectedItemChange,
-        keySelector = keySelector,
-        modifier = modifier,
-        text = { Text(text = displayedText(selectedItem), style = displayedTextStyle) },
-        enabled = enabled,
-        contentPadding = contentPadding,
-        padding = padding,
-        shape = shape,
-        colors = colors,
-    )
+    val selectedKey = remember(selectedItem) { keySelector(selectedItem) }
+
+    Column(modifier = modifier, verticalArrangement = contentPadding) {
+        source.forEach { item ->
+            val itemKey = keySelector(item)
+
+            key(itemKey) {
+                RadioButtonWithText(
+                    isSelected = selectedKey == itemKey,
+                    onClick = { onSelectedItemChange(item) },
+                    text = text,
+                    isEnabled = enabled,
+                    padding = padding,
+                    shape = shape,
+                    colors = colors,
+                )
+            }
+        }
+    }
 }
--- bereza-reactive/src/commonMain/kotlin/com/github/ravenzip/berezaUI/reactive/components/radio/RadioGroup.kt
+++ bereza-reactive/src/commonMain/kotlin/com/github/ravenzip/berezaUI/reactive/components/radio/RadioGroup.kt
@@ -1,19 +1,20 @@
 package com.github.ravenzip.berezaUI.reactive.components.radio
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.RadioButtonColors
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.key
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.github.ravenzip.berezaUI.core.components.radio.RadioButtonWithText
+import androidx.compose.ui.unit.sp
+import com.github.ravenzip.berezaUI.core.components.radio.RadioGroup
+import com.github.ravenzip.berezaUI.extensions.components.radio.RadioGroup
 import com.github.ravenzip.kotlinreactiveforms.data.isEnabled
 import com.github.ravenzip.kotlinreactiveforms.form.MutableFormControl
 
@@ -23,8 +24,7 @@ fun <T, K : Any> RadioGroup(
     source: List<T>,
     keySelector: (T) -> K,
     text: @Composable (T) -> Unit,
-    groupModifier: Modifier = Modifier,
-    itemModifier: Modifier = Modifier,
+    modifier: Modifier = Modifier,
     contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
     padding: PaddingValues = PaddingValues(15.dp),
     shape: Shape = RoundedCornerShape(14.dp),
@@ -32,27 +32,57 @@ fun <T, K : Any> RadioGroup(
 ) {
     val value = control.valueChanges.collectAsState().value
     val status = control.statusChanges.collectAsState().value
-    val selectedKey = remember(value) { keySelector(value) }
 
-    Column(modifier = groupModifier, verticalArrangement = contentPadding) {
-        source.forEach { item ->
-            val itemKey = keySelector(item)
+    RadioGroup(
+        source = source,
+        selectedItem = value,
+        onSelectedItemChange = { value ->
+            control.setValue(value)
+            control.markAsDirty()
+        },
+        keySelector = keySelector,
+        text = text,
+        enabled = status.isEnabled(),
+        modifier = modifier,
+        contentPadding = contentPadding,
+        padding = padding,
+        shape = shape,
+        colors = colors,
+    )
+}
 
-            key(itemKey) {
-                RadioButtonWithText(
-                    isSelected = selectedKey == itemKey,
-                    onClick = {
-                        control.setValue(item)
-                        control.markAsDirty()
-                    },
-                    text = { text(item) },
-                    isEnabled = status.isEnabled(),
-                    modifier = itemModifier,
-                    padding = padding,
-                    shape = shape,
-                    colors = colors,
-                )
-            }
-        }
-    }
+@Composable
+fun <T, K : Any> RadioGroup(
+    control: MutableFormControl<T>,
+    source: List<T>,
+    keySelector: (T) -> K,
+    displayedText: (T) -> String,
+    displayedTextStyle: TextStyle =
+        TextStyle.Default.merge(fontSize = 16.sp, fontWeight = FontWeight.Medium),
+    modifier: Modifier = Modifier,
+    contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
+    padding: PaddingValues = PaddingValues(15.dp),
+    shape: Shape = RoundedCornerShape(14.dp),
+    colors: RadioButtonColors = RadioButtonDefaults.colors(),
+) {
+    val value = control.valueChanges.collectAsState().value
+    val status = control.statusChanges.collectAsState().value
+
+    RadioGroup(
+        source = source,
+        selectedItem = value,
+        onSelectedItemChange = { value ->
+            control.setValue(value)
+            control.markAsDirty()
+        },
+        keySelector = keySelector,
+        displayedText = displayedText,
+        displayedTextStyle = displayedTextStyle,
+        enabled = status.isEnabled(),
+        modifier = modifier,
+        contentPadding = contentPadding,
+        padding = padding,
+        shape = shape,
+        colors = colors,
+    )
 }
