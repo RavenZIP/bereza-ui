2cc3b2c6a9902b40471320d76a279baadeec4a38
Черных Александр <razor01478920@mail.ru>
2026-04-17 09:48:26 +0300

    Добавил CheckboxGroup
    Не окончательный вариант, но работает

--- bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/components/checkbox/CheckboxGroup.kt
+++ bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/components/checkbox/CheckboxGroup.kt
@@ -1,3 +1,69 @@
 package com.github.ravenzip.berezaUI.core.components.checkbox
 
-// TODO
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun <T, K : Any> BasicColumnGroup(
+    source: List<T>,
+    keySelector: (T) -> K,
+    modifier: Modifier = Modifier,
+    contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
+    content: @Composable (K) -> Unit,
+) {
+    Column(modifier = modifier, verticalArrangement = contentPadding) {
+        source.forEach { item ->
+            val itemKey = keySelector(item)
+
+            key(itemKey) { content(itemKey) }
+        }
+    }
+}
+
+@Composable
+fun <T, K : Any> CheckboxGroup(
+    source: List<T>,
+    selectedItems: List<T>,
+    onSelectedItemChange: (T) -> Unit,
+    keySelector: (T) -> K,
+    modifier: Modifier = Modifier,
+    text: @Composable (T) -> Unit,
+    enabled: Boolean = true,
+    contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
+    padding: PaddingValues = PaddingValues(15.dp),
+    shape: Shape = RoundedCornerShape(14.dp),
+    colors: CheckboxColors = CheckboxDefaults.colors(),
+) {
+    val sourceMap = remember(source) { source.associateBy(keySelector) }
+    val selectedItemsMap = remember(selectedItems) { selectedItems.associateBy(keySelector) }
+
+    BasicColumnGroup(
+        source = source,
+        keySelector = keySelector,
+        modifier = modifier,
+        contentPadding = contentPadding,
+    ) { itemKey ->
+        val isSelected = selectedItemsMap.contains(itemKey)
+        val item = sourceMap.getValue(itemKey)
+
+        CheckboxWithText(
+            isSelected = isSelected,
+            onClick = { onSelectedItemChange(item) },
+            text = { text(item) },
+            enabled = enabled,
+            padding = padding,
+            shape = shape,
+            colors = colors,
+        )
+    }
+}
---
+++ bereza-reactive/src/commonMain/kotlin/com/github/ravenzip/berezaUI/reactive/components/checkbox/CheckboxGroup.kt
@@ -0,0 +1,52 @@
+package com.github.ravenzip.berezaUI.reactive.components.checkbox
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.github.ravenzip.berezaUI.core.components.checkbox.CheckboxGroup
+import com.github.ravenzip.berezaUI.core.utils.collectAsStateLifecycleAware
+import com.github.ravenzip.kotlinreactiveforms.data.isEnabled
+import com.github.ravenzip.kotlinreactiveforms.form.MutableFormControl
+
+@Composable
+fun <T, K : Any> CheckboxGroup(
+    control: MutableFormControl<List<T>>,
+    source: List<T>,
+    keySelector: (T) -> K,
+    modifier: Modifier = Modifier,
+    text: @Composable (T) -> Unit,
+    contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
+    padding: PaddingValues = PaddingValues(15.dp),
+    shape: Shape = RoundedCornerShape(14.dp),
+    colors: CheckboxColors = CheckboxDefaults.colors(),
+) {
+    val selectedItems by control.valueChanges.collectAsStateLifecycleAware()
+    val status by control.statusChanges.collectAsStateLifecycleAware()
+
+    CheckboxGroup(
+        source = source,
+        selectedItems = selectedItems,
+        onSelectedItemChange = { value ->
+            val selectedItems =
+                if (value in control.value) control.value - value else control.value + value
+
+            control.setValue(selectedItems)
+            control.markAsTouched()
+        },
+        keySelector = keySelector,
+        modifier = modifier,
+        text = text,
+        enabled = status.isEnabled(),
+        contentPadding = contentPadding,
+        padding = padding,
+        shape = shape,
+        colors = colors,
+    )
+}
