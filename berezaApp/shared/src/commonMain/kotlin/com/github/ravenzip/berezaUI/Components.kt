13e5679e048f03eac4196ded8402d2160d75186f
RavenZIP <razor01478920@mail.ru>
2026-09-01 06:31:04 +0300

    тудушка

--- berezaApp/shared/src/commonMain/kotlin/com/github/ravenzip/berezaUI/Components.kt
+++ berezaApp/shared/src/commonMain/kotlin/com/github/ravenzip/berezaUI/Components.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
+// TODO если это будет в каком-то отдельном проекте, то подключить его и удалить это
 @Composable
 fun SimpleButton(onClick: () -> Unit, text: String) {
     Button(
