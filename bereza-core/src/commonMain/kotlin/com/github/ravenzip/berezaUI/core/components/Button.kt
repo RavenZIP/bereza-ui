2e09a2ca3f604ae0b225b8719392db02a40e557e
RavenZIP <razor01478920@mail.ru>
2026-09-13 13:51:45 +0300

    Удалил IconButton (бесполезен)

--- bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/components/Button.kt
+++ bereza-core/src/commonMain/kotlin/com/github/ravenzip/berezaUI/core/components/Button.kt
@@ -1,8 +1,9 @@
 package com.github.ravenzip.berezaUI.core.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -11,39 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
-import com.github.ravenzip.berezaUI.core.components.layout.RoundedBox
-
-@Composable
-fun IconButton(
-    onClick: () -> Unit,
-    icon: @Composable () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    shape: Shape = RoundedCornerShape(14.dp),
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
-    interactionSource: MutableInteractionSource? = null,
-    contentPadding: PaddingValues = PaddingValues(10.dp),
-) {
-    val containerColor = if (enabled) colors.containerColor else colors.disabledContainerColor
-
-    RoundedBox(
-        modifier =
-            Modifier.clickable(
-                    onClick = onClick,
-                    enabled = enabled,
-                    role = Role.Button,
-                    interactionSource = interactionSource,
-                )
-                .padding(contentPadding)
-                .then(modifier),
-        shape = shape,
-        backgroundColor = containerColor,
-    ) {
-        icon()
-    }
-}
 
 @Composable
 fun RichButton(
